4438df49ecb08f03388783e849402d86a73220f5
DeadlySquad13 <46250621+DeadlySquad13@users.noreply.github.com>
2024-04-13 12:46:37 +0300

    [Enh](Markdown): Added treesitter support to markdown.

--- lua/deadly-gruv/languages/markdown.lua
+++ lua/deadly-gruv/languages/markdown.lua
@@ -5,7 +5,9 @@ local mc = require("deadly-gruv.colors").mc
 local sp = require("deadly-gruv.colors").sp
 local base = require("deadly-gruv.base")
 
-local M = lush(function()
+local M = lush(function(injected_functions)
+  local sym = injected_functions.sym
+
   return {
     markdownH1({ fg = mc.MarkdownHeaders[1], gui = base.Title.gui }), -- Default value just need it for transformations below.
     markdownH2({ fg = mc.MarkdownHeaders[2], gui = markdownH1.gui }),
@@ -13,6 +15,37 @@ local M = lush(function()
     markdownH4({ fg = mc.MarkdownHeaders[4], gui = markdownH1.gui }),
     markdownH5({ fg = mc.MarkdownHeaders[5], gui = markdownH1.gui }),
     markdownH6({ fg = mc.MarkdownHeaders[6], gui = markdownH1.gui }),
+
+    markdownOrderedListMarker({ fg = mc.ImportantFlowControlStatements[1] }),
+
+    markdownUrl({ fg = mc.ConstantValues[1] }),
+    markdownUrlTitle({ fg = mc.Annotations[1] }),
+
+    -- TODO: remove .markdown to make it markup style agnostic?
+    -- Treesitter.
+    sym'@markup.heading.1.markdown'({ markdownH1 }), -- Default value just need it for transformations below.
+    sym'@markup.heading.2.markdown'({ markdownH2 }),
+    sym'@markup.heading.3.markdown'({ markdownH3 }),
+    sym'@markup.heading.4.markdown'({ markdownH4 }),
+    sym'@markup.heading.5.markdown'({ markdownH5 }),
+    sym'@markup.heading.6.markdown'({ markdownH6 }),
+
+    sym'@markup.heading.1.marker.markdown'({ fg = markdownH1.fg.lighten(2*delta).desaturate(14*delta) }), -- Default value just need it for transformations below.
+    sym'@markup.heading.2.marker.markdown'({ fg = markdownH2.fg.lighten(2*delta).desaturate(14*delta) }),
+    sym'@markup.heading.3.marker.markdown'({ fg = markdownH3.fg.lighten(2*delta).desaturate(14*delta) }),
+    sym'@markup.heading.4.marker.markdown'({ fg = markdownH4.fg.lighten(2*delta).desaturate(14*delta) }),
+    sym'@markup.heading.5.marker.markdown'({ fg = markdownH5.fg.lighten(2*delta).desaturate(14*delta) }),
+    sym'@markup.heading.6.marker.markdown'({ fg = markdownH6.fg.lighten(2*delta).desaturate(14*delta) }),
+
+    sym'@markup.list.markdown'({ markdownOrderedListMarker }),
+
+    sym'@markup.link.url.markdown'({ markdownUrl }),
+    sym'@markup.link.label.markdown'({ markdownUrlTitle }),
+
+    sym'@markup.strong.markdown_inline'({ gui = 'bold' }),
+    sym'@markup.italic.markdown_inline'({ gui = 'italic' }),
+
+    sym'@markup.raw.markdown_inline'({ base.ColorColumn }),
   }
 end)
 
