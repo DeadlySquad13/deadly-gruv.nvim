84c65a8400eaabb3fd8f81b141b96a710c0b643b
DeadlySquad13 <46250621+DeadlySquad13@users.noreply.github.com>
2024-06-26 14:45:42 +0000

    [Enh](Json): Added treesitter groups.

--- lua/deadly-gruv/languages/json.lua
+++ lua/deadly-gruv/languages/json.lua
@@ -3,19 +3,44 @@ local lush = require('lush')
 local delta = require('deadly-gruv.constants').delta;
 local mc = require('deadly-gruv.colors').mc;
 
-local M = lush(function()
+local base = require('deadly-gruv.base');
+
+-- TODO: Move all used base.Something into mega_groups.
+-- Add modified variants of base groups as meta_groups too.
+local M = lush(function(injected_functions)
+  local sym = injected_functions.sym
+
   return {
     -- * Keys.
-    jsonKeyword { fg = mc.Symbols[4] }, -- Matches @field from treesitter.
+    jsonKeyword { fg = mc.Symbols[4].darken(delta).saturate(delta) },
+    sym'@property.json' { jsonKeyword },
     -- * Values.
-     jsonBoolean        { fg = mc.Symbols[6] }, --   A nboolean constant.
-     jsonNumber         { fg = mc.ClassSymbols[2] }, --   A number constant.
-     jsonString         { fg = mc.Symbols[5] }, -- A string constant: "this is a string". Matches @variable from treesitter instead of string because it's used a lot.
+    -- jsonBoolean { fg = mc.Symbols[6] },              --   A boolean constant.
+    -- jsonNumber { fg = mc.ClassSymbols[2] },          --   A number constant.
+    -- jsonString { fg = mc.Symbols[5] },               -- A string constant: "this is a string". Matches @variable from treesitter instead of string because it's used a lot.
+    -- - A boolean constant.
+    jsonBoolean { fg = base.Boolean.fg.darken(3*delta) },
+    sym'@boolean.json' { jsonBoolean },
+    -- - A number constant.
+    jsonNumber { fg = base.Number.fg.darken(2*delta).desaturate(delta) },
+    sym'@number.json' { jsonNumber },
+    -- - A string constant: "this is a string". Matches @variable from treesitter instead of string because it's used a lot.
+    jsonString { fg = base.String.fg.darken(2*delta) },
+    sym'@string.json' { jsonString },
 
     -- * Punctuation.
-     jsonKeywordMatch         { fg = mc.Punctuation[1] }, -- A keyword *including* "" and : (I use it for latest).
-     jsonNoise         { fg = jsonKeywordMatch.fg }, --   Comma at the end of the statement.
-     jsonQuote         { fg = mc.Punctuation[2] }, --   A quote embracing both the keyword and value.
+    -- - A keyword *including* "" and : (I use it for latest).
+    jsonKeywordMatch { fg = mc.Punctuation[1] },
+    -- - Comma at the end of the statement.
+    jsonNoise { fg = jsonKeywordMatch.fg },
+
+    -- No direct equivalent in treesitter for the above groups. Delimiter is
+    -- used for both ':' and ','.
+    sym'@punctuation.delimiter.json' { jsonKeywordMatch },
+
+    -- - A quote embracing both the keyword and value.
+    jsonQuote { fg = mc.Punctuation[2] },
+    sym'@conceal.json' { jsonQuote },
   }
 end)
 
